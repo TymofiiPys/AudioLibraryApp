2b4de7eb4e8b094647feafde7a50a800c8790e0e
TymofiiPys <nfstimoha2004@gmail.com>
2024-09-22 21:42:51 +0300

    Upload backend and frontend template
    Upload templates for further work on them

---
+++ Backend/AudioLibBE/src/main/java/org/audiolib/audiolib/AudioLibApplication.java
@@ -0,0 +1,13 @@
+package org.audiolib.audiolib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioLibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AudioLibApplication.class, args);
+    }
+
+}
