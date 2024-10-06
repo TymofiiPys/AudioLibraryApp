5a6ce48948f191e74fbe260df69583cd84b54c8b
TymofiiPys <nfstimoha2004@gmail.com>
2024-10-06 19:23:02 +0300

    Authorization

--- Frontend/audiolib-fe/src/App.js
+++ Frontend/audiolib-fe/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useAuth0 } from "@auth0/auth0-react";
+import "./App.css";
+import { Router, Route, Routes } from "react-router-dom";
+import LoginButton from "./auth/login";
+import LogoutButton from "./auth/logout";
+import Profile from "./profile/profile";
 
 function App() {
+  const { isAuthenticated } = useAuth0();
+  const LogInOutBtn = isAuthenticated ? LogoutButton : LoginButton;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        Привіт! я чисто тестую логін. завтра зроблю більше фронта. на кнопочку
+        <LogInOutBtn />
+        <Routes>
+          <Route path="/profile" element={Profile}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- Frontend/audiolib-fe/src/index.js
+++ Frontend/audiolib-fe/src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="dev-pz3q1truzjwp6lbi.us.auth0.com"
+    clientId="0A4cWgMmPyDZiQO1dCkD81NyUH1LUWSf"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
-  </React.StrictMode>
+  </Auth0Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
