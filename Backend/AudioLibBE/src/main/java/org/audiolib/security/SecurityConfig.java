35ac42d790bb0485d63483d48873dfa2ab259915
TymofiiPys <nfstimoha2004@gmail.com>
2024-10-06 20:32:23 +0300

    Authorization, backend (commented)
    Need to uncomment SecurityConfig and add okta to pom.xml when all done

---
+++ Backend/AudioLibBE/src/main/java/org/audiolib/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package org.audiolib.security;
+
+import org.springframework.context.annotation.Bean;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//import static org.springframework.security.config.Customizer.withDefaults;
+
+//@EnableWebSecurity
+public class SecurityConfig {
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        return http
+//                .authorizeHttpRequests((authorize) -> authorize
+//                        .requestMatchers("/api/public").permitAll()
+//                        .requestMatchers("/api/private").authenticated()
+//                        .requestMatchers("/api/private-scoped").hasAuthority("SCOPE_read:messages")
+//                )
+//                .cors(withDefaults())
+//                .oauth2ResourceServer(oauth2 -> oauth2
+//                        .jwt(withDefaults())
+//                )
+//                .build();
+//    }
+}
