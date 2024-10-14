fbfbe90657fbbd5a26639fff4ca99aa611603ab9
TymofiiPys <nfstimoha2004@gmail.com>
2024-10-14 20:10:11 +0300

    History primary key fix

--- Backend/AudioLibBE/src/main/java/org/audiolib/entity/HistoryPK.java
+++ Backend/AudioLibBE/src/main/java/org/audiolib/entity/HistoryPK.java
@@ -1,6 +1,14 @@
 package org.audiolib.entity;
 
+import lombok.*;
+
 import java.io.Serializable;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class HistoryPK implements Serializable {
+    private String userId;
+    private long audioId;
 }
